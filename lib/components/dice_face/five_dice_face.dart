e8c1a77256a1e47a9ac9ddf8b483980b2cc8fd60
José Azevedo <jose@espressolabs.com.br>
2023-08-22 20:17:25 -0300

    feat: implement FiveDiceFace transitions

--- lib/components/dice_face/five_dice_face.dart
+++ lib/components/dice_face/five_dice_face.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_dice/components/dots/two_and_four_dots.dart';
+import 'package:smooth_dice/components/dots/two_and_six_dots.dart';
+import 'package:smooth_dice/components/dots/two_opposite_dots.dart';
 
 import '../dice.dart';
 import '../dots/dice_dot.dart';
@@ -11,64 +14,90 @@ class FiveDiceFace extends DiceFace {
   int get faceValue => 5;
 
   @override
-  Widget build(BuildContext context) {
-    return const SizedBox.expand(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              DiceDot(),
-              DiceDot(),
-            ],
-          ),
-          DiceDot(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              DiceDot(),
-              DiceDot(),
-            ],
-          ),
-        ],
-      ),
+  Widget fromOne(DiceProperties diceProperties) {
+    return const TwoOppositeDots(
+      children: [
+        TwoOppositeDots(
+          dotsPosition: DotsPosition.topRightBottomLeft,
+        ),
+        DiceDot(),
+      ],
+    );
+  }
+
+  @override
+  Widget fromTwo(DiceProperties diceProperties) {
+    return const TwoAndFourDots(
+      dotsEndResult: DotsEndResult.smaller,
+      transitionAxis: Axis.horizontal,
+      children: [
+        TwoOppositeDots(
+          animationDirection: AnimationDirection.inwards,
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget fromThree(DiceProperties diceProperties) {
+    return const TwoAndFourDots(
+      dotsEndResult: DotsEndResult.smaller,
+      transitionAxis: Axis.horizontal,
+      children: [
+        TwoOppositeDots(
+          animationDirection: AnimationDirection.outwards,
+          dotsPosition: DotsPosition.topRightBottomLeft,
+        ),
+        DiceDot(),
+      ],
+    );
+  }
+
+  @override
+  Widget fromFour(DiceProperties diceProperties) {
+    return const TwoAndFourDots(
+      dotsEndResult: DotsEndResult.smaller,
+      transitionAxis: Axis.horizontal,
+      children: [
+        TwoOppositeDots(
+          animationDirection: AnimationDirection.inwards,
+        ),
+        TwoOppositeDots(
+          animationDirection: AnimationDirection.inwards,
+          dotsPosition: DotsPosition.topRightBottomLeft,
+        ),
+      ],
     );
   }
 
   @override
   Widget fromFive(DiceProperties diceProperties) {
-    // TODO: implement toFive
-    throw UnimplementedError();
-  }
-
-  @override
-  Widget fromFour(DiceProperties diceProperties) {
-    // TODO: implement toFour
-    throw UnimplementedError();
-  }
-
-  @override
-  Widget fromOne(DiceProperties diceProperties) {
-    // TODO: implement toOne
-    throw UnimplementedError();
+    return const TwoAndFourDots(
+      dotsEndResult: DotsEndResult.smaller,
+      children: [
+        TwoAndFourDots(),
+        TwoOppositeDots(
+          animationDirection: AnimationDirection.inwards,
+        ),
+        DiceDot(),
+      ],
+    );
   }
 
   @override
   Widget fromSix(DiceProperties diceProperties) {
-    // TODO: implement toSix
-    throw UnimplementedError();
-  }
-
-  @override
-  Widget fromThree(DiceProperties diceProperties) {
-    // TODO: implement toThree
-    throw UnimplementedError();
-  }
-
-  @override
-  Widget fromTwo(DiceProperties diceProperties) {
-    // TODO: implement toTwo
-    throw UnimplementedError();
+    return const TwoAndSixDots(
+      dotsEndResult: DotsEndResult.larger,
+      children: [
+        TwoAndFourDots(dotsEndResult: DotsEndResult.smaller),
+        TwoOppositeDots(
+          animationDirection: AnimationDirection.inwards,
+        ),
+        TwoOppositeDots(
+          animationDirection: AnimationDirection.inwards,
+          dotsPosition: DotsPosition.topRightBottomLeft,
+        ),
+      ],
+    );
   }
 }
