3a6edb3fdd35ecf6353c8e5edc2d53ca1d6cee3a
José Azevedo <jose@espressolabs.com.br>
2023-08-22 21:31:07 -0300

    feat: implement SixDiceFace transitions

--- lib/components/dice_face/six_dice_face.dart
+++ lib/components/dice_face/six_dice_face.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_dice/components/dots/two_and_four_dots.dart';
+import 'package:smooth_dice/components/dots/two_and_six_dots.dart';
 
 import '../dice.dart';
 import '../dots/dice_dot.dart';
+import '../dots/two_opposite_dots.dart';
 import 'dice_face.dart';
 
 class SixDiceFace extends DiceFace {
@@ -9,33 +12,67 @@ class SixDiceFace extends DiceFace {
 
   @override
   int get faceValue => 6;
-
   @override
-  Widget build(BuildContext context) {
-    return const SizedBox.expand(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  Widget fromOne(DiceProperties diceProperties) {
+    final tween = Tween<double>(begin: diceProperties.maxOffset.dx / 2, end: 0);
+
+    return SizedBox.expand(
+      child: Stack(
+        alignment: Alignment.center,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              DiceDot(),
-              DiceDot(),
-            ],
+          TweenAnimationBuilder(
+            tween: tween,
+            duration: diceProperties.duration,
+            builder: (_, position, __) => Positioned(
+              top: position,
+              left: position,
+              child: const DiceDot(),
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              DiceDot(),
-              DiceDot(),
-            ],
+          TweenAnimationBuilder(
+            tween: tween,
+            duration: diceProperties.duration,
+            builder: (_, position, __) => Positioned(
+              top: position,
+              right: position,
+              child: const DiceDot(),
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              DiceDot(),
-              DiceDot(),
-            ],
+          TweenAnimationBuilder(
+            tween: tween,
+            duration: diceProperties.duration,
+            builder: (_, position, __) => Positioned(
+              bottom: position,
+              right: position,
+              child: const DiceDot(),
+            ),
+          ),
+          TweenAnimationBuilder(
+            tween: tween,
+            duration: diceProperties.duration,
+            builder: (_, position, __) => Positioned(
+              bottom: position,
+              left: position,
+              child: const DiceDot(),
+            ),
+          ),
+          TweenAnimationBuilder(
+            tween: tween,
+            duration: diceProperties.duration,
+            builder: (_, position, __) => Positioned(
+              bottom: diceProperties.maxOffset.dy / 2,
+              left: position,
+              child: const DiceDot(),
+            ),
+          ),
+          TweenAnimationBuilder(
+            tween: tween,
+            duration: diceProperties.duration,
+            builder: (_, position, __) => Positioned(
+              bottom: diceProperties.maxOffset.dy / 2,
+              right: position,
+              child: const DiceDot(),
+            ),
           ),
         ],
       ),
@@ -43,38 +80,119 @@ class SixDiceFace extends DiceFace {
   }
 
   @override
-  Widget fromFive(DiceProperties diceProperties) {
-    // TODO: implement toFive
-    throw UnimplementedError();
-  }
-
-  @override
-  Widget fromFour(DiceProperties diceProperties) {
-    // TODO: implement toFour
-    throw UnimplementedError();
-  }
-
-  @override
-  Widget fromOne(DiceProperties diceProperties) {
-    // TODO: implement toOne
-    throw UnimplementedError();
-  }
-
-  @override
-  Widget fromSix(DiceProperties diceProperties) {
-    // TODO: implement toSix
-    throw UnimplementedError();
+  Widget fromTwo(DiceProperties diceProperties) {
+    return const TwoAndSixDots(
+      dotsEndResult: DotsEndResult.smaller,
+    );
   }
 
   @override
   Widget fromThree(DiceProperties diceProperties) {
-    // TODO: implement toThree
-    throw UnimplementedError();
+    return const TwoAndSixDots(
+      dotsEndResult: DotsEndResult.smaller,
+      children: [
+        TwoOppositeDots(),
+      ],
+    );
   }
 
   @override
-  Widget fromTwo(DiceProperties diceProperties) {
-    // TODO: implement toTwo
-    throw UnimplementedError();
+  Widget fromFour(DiceProperties diceProperties) {
+    return const TwoAndSixDots(
+      dotsEndResult: DotsEndResult.smaller,
+      children: [
+        Positioned(
+          top: 0,
+          right: 0,
+          child: DiceDot(),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          child: DiceDot(),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget fromFive(DiceProperties diceProperties) {
+    return const TwoAndSixDots(
+      dotsEndResult: DotsEndResult.smaller,
+      children: [
+        TwoAndFourDots(dotsEndResult: DotsEndResult.larger),
+        TwoOppositeDots(),
+        TwoOppositeDots(
+          dotsPosition: DotsPosition.topRightBottomLeft,
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget fromSix(DiceProperties diceProperties) {
+    final tween = Tween<double>(begin: 0, end: diceProperties.maxOffset.dx);
+
+    return SizedBox.expand(
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          TweenAnimationBuilder(
+            tween: tween,
+            duration: diceProperties.duration,
+            builder: (_, position, __) => Positioned(
+              top: position,
+              left: position,
+              child: const DiceDot(),
+            ),
+          ),
+          TweenAnimationBuilder(
+            tween: tween,
+            duration: diceProperties.duration,
+            builder: (_, position, __) => Positioned(
+              top: position,
+              right: position,
+              child: const DiceDot(),
+            ),
+          ),
+          TweenAnimationBuilder(
+            tween: tween,
+            duration: diceProperties.duration,
+            builder: (_, position, __) => Positioned(
+              bottom: position,
+              right: position,
+              child: const DiceDot(),
+            ),
+          ),
+          TweenAnimationBuilder(
+            tween: tween,
+            duration: diceProperties.duration,
+            builder: (_, position, __) => Positioned(
+              bottom: position,
+              left: position,
+              child: const DiceDot(),
+            ),
+          ),
+          TweenAnimationBuilder(
+            tween: tween,
+            duration: diceProperties.duration,
+            builder: (_, position, __) => Positioned(
+              bottom: diceProperties.maxOffset.dy / 2,
+              left: position,
+              child: const DiceDot(),
+            ),
+          ),
+          TweenAnimationBuilder(
+            tween: tween,
+            duration: diceProperties.duration,
+            builder: (_, position, __) => Positioned(
+              bottom: diceProperties.maxOffset.dy / 2,
+              right: position,
+              child: const DiceDot(),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
